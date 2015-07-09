3b11beee91cc65c898b3f826abfc7790df71e18d
wangyazhen <wangyazhen224@163.com>
2015-07-09 13:14:02 +0800

    add 404

---
+++ node/es601.js
@@ -0,0 +1,14 @@
+
+const hello = "你好 我是传奇";
+
+console.log(hello);
+
+// var [one, two, three] = ['one1', 'two2', 'three3'];
+// console.log(one, two, three);
+
+//var [head, ...tail] = [1, 2, 3, 4];  
+//console.log(tail);
+
+var map = new Map();
+map.set('123');
+console.log(map.size);
